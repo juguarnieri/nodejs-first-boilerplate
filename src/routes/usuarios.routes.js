51fe61a682b3d55db4d3edcdf644441d0804f761
Júla Guarnieri <juliaguarnieri04@gmail.com>
2024-10-24 11:40:27 -0300

    feat: create get all users

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -3,5 +3,14 @@ import { Router } from "express";
 const usuariosRoutes = Router();
 
 let usuarios = [];
+// Array com usuários pré-cadastrados
+
+// Rota para listar todos os usuários
+usuariosRoutes.get("/", (req, res) => {
+    return res.status(200).json({
+       message: usuarios.length == 0 ?"Não há usuários cadastrados" :`Total de usuários: ${usuarios.length}`,usuarios 
+  });
+});
+  
 
 export default usuariosRoutes;
